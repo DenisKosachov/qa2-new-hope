fc3117bb6f0a8352b553b6076b08d04a97df906d
DenisKosachov <kosachovdenis@gmail.com>
2021-08-27 14:26:03 +0300

    QA2 Lesson 17

---
+++ src/test/java/pageobject/tickets/pages/PassengerInfoPage.java
@@ -0,0 +1,42 @@
+package pageobject.tickets.pages;
+
+import model.Reservation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pageobject.BaseFunc;
+
+import java.util.List;
+
+public class PassengerInfoPage {
+    private final By AIRPORT_NAME = By.xpath(".//span[@class = 'bTxt']");
+
+    private final By NAME = By.id("name");
+    private final By SURNAME = By.id("surname");
+    private final By DISCOUNT = By.id("discount");
+    private final By ADULTS = By.id("adults");
+    private final By CHILDREN = By.id("children");
+    private final By LUGGAGE = By.id("bugs");
+    private final By FLIGHT = By.id("flight");
+    private final By GET_PRICE_LINK = By.xpath(".//div[@id = 'fullForm']/span[@style = 'cursor: pointer;']");
+
+    private BaseFunc baseFunc;
+
+    public PassengerInfoPage(BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+
+    public List<WebElement> getAirports() {
+        return baseFunc.findElements(AIRPORT_NAME);
+    }
+
+    public void submitPassengerInfo(Reservation reservation) {
+        baseFunc.type(NAME, reservation.getName());
+        baseFunc.type(SURNAME, reservation.getSurname());
+        baseFunc.type(DISCOUNT, reservation.getDiscount());
+        baseFunc.type(ADULTS, reservation.getAdults());
+        baseFunc.type(CHILDREN, reservation.getChildren());
+        baseFunc.type(LUGGAGE, reservation.getBugs());
+        baseFunc.select(FLIGHT, reservation.getFullDate());
+        baseFunc.click(GET_PRICE_LINK);
+    }
+}
